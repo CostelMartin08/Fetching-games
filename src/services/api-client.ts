676903b2385c6141875c9ef668f02f793ad0b461
Costel <costelmartinescu2000@gmail.com>
2023-09-16 11:47:30 +0300

    Refactoring

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "c7b18323a47d40c394ea5b019646b1f5",
+    key: "196e464d40814c45bd598112ce430167",
   },
 });
