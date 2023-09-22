93c28b96668233582b2b98402d9af6b706d7fc92
Costel <costelmartinescu2000@gmail.com>
2023-09-22 11:08:34 +0300

    Setup routing

---
+++ src/pages/GameDetailPage.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+
+const GameDetailPage = () => {
+
+
+
+
+    return (
+
+        <>
+        <h2>GameDetailPage
+
+        </h2>
+        </>
+    )
+}
+
+export default GameDetailPage;
---
+++ src/pages/Layout.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import NavBar from '../components/NavBar';
+import { Outlet } from 'react-router-dom';
+
+
+const Layout = () => {
+
+
+
+    return (
+        <>
+            <NavBar />
+            <Outlet />
+        </>
+    )
+}
+
+export default Layout;
